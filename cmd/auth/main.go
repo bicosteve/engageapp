714e3a5074b77c87e5f742ec29aa88fd1e8cdefc
bicosteve <bicosteve4@gmail.com>
2024-05-12 06:33:57 +0300

    removed main package init func

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"os"
-
 	"github.com/engageapp/pkg/controllers"
-	"github.com/engageapp/pkg/utils"
-	"github.com/joho/godotenv"
 )
 
-var base controllers.Base
-
-func init() {
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		utils.Log("ERROR", "env", "error loading env file because of %v", err)
-		os.Exit(1)
-	}
-
-	base.RunAuth()
-
-}
-
 func main() {
+
+	var base controllers.Base
+
 	base.Init()
+	base.RunAuth()
 }
