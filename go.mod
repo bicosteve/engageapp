1fd87e16e375d51082414cecf588b5e9c5678c16
bsteve <bicosteve4@gmail.com>
2024-06-30 08:18:47 +0300

    added user test

--- go.mod
+++ go.mod
@@ -12,4 +12,10 @@ require (
 	golang.org/x/crypto v0.23.0
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- pkg/controllers/user_test.go
+++ pkg/controllers/user_test.go
@@ -1,1 +1,40 @@
 package controllers
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var base Base
+
+func TestCreateUser(t *testing.T) {
+	payload := []byte(`{"email":"bico.steve@gmail.com","password":"1234","confirm_password":"1234"}`)
+
+	req, err := http.NewRequest("POST", "/register", bytes.NewBuffer(payload))
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder() // rr-> response recorder
+	handler := http.HandlerFunc(base.PostUser)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusCreated, rr.Code)
+
+}
+
+func TestInvalidCredentials(t *testing.T) {
+	// Simulating invalid json
+	payload := []byte(`{"email":"bico.steve@gmail.com","password":"1234"}`)
+	request, err := http.NewRequest("POST", "/register", bytes.NewBuffer(payload))
+	assert.NoError(t, err)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(base.PostUser)
+
+	handler.ServeHTTP(responseRecorder, request)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+}
